1e90180e1fdf551585f7646bf51e9e61afca2229
jeremygholland <jeremygholland@gmail.com>
2018-02-25 11:40:20 -0600

    got radius search working

--- js/app.js
+++ js/app.js
@@ -8,12 +8,11 @@ app.controller('myCtrl', ['$scope', '$http', function ($scope, $http){
 $.ajax
 			({
 				type: "GET",
-				url: 'https://api.brewerydb.com/v2/location/d25euF?key=4b50655001c2875f2ef1e4cf9dc31c6c&format=json',
+				url: 'https://api.brewerydb.com/v2/search/geo/point?lat=35.772096&lng=-78.638614&radius=30&units=m&key=4b50655001c2875f2ef1e4cf9dc31c6c&format=json',
 				dataType: 'json',
 				success: function(json){
 
-		  		console.log(json)
-		  	}
+console.log(json.data[0].id)		  	}
 		  })
 		}
 
