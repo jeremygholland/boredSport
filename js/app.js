1bc07cb8c12a57b581f9dd5f7d52c7843dab8d18
jeremygholland <jeremygholland@gmail.com>
2018-02-25 15:20:28 -0600

    working for 30 mile radius of austin

--- js/app.js
+++ js/app.js
@@ -3,17 +3,23 @@ var app = angular.module('app', [])
 app.controller('myCtrl', ['$scope', '$http', function ($scope, $http){
 	$scope.test =  (5*9);
 	console.log($scope.test);
+	var nameArr =[];
 
 	$scope.btnClick=function(){
 $.ajax
 			({
 				type: "GET",
-				url: 'https://api.brewerydb.com/v2/search/geo/point?lat=35.772096&lng=-78.638614&radius=30&units=m&key=4b50655001c2875f2ef1e4cf9dc31c6c&format=json',
+				url: 'https://api.brewerydb.com/v2/search/geo/point?lat=30.2672&lng=-97.7431&radius=30&units=m&key=4b50655001c2875f2ef1e4cf9dc31c6c&format=json',
 				dataType: 'json',
 				success: function(json){
-
-console.log(json.data[0].id)		  	}
-		  })
+for (i =0; i<json.data.length; i ++){
+nameArr.push(json.data[i].brewery.name)		 	}
+		  }
+		})
 		}
+$scope.consoleLog=function(){
+	console.log(nameArr)
+}
+
 
 }])
