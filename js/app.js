a2911d5e0519d15c0902bc9dc161b0d8b3289bd7
jeremygholland <jeremygholland@gmail.com>
2018-02-25 15:43:41 -0600

    added functionality to find names and long and lat

--- js/app.js
+++ js/app.js
@@ -13,13 +13,27 @@ $.ajax
 				dataType: 'json',
 				success: function(json){
 for (i =0; i<json.data.length; i ++){
-nameArr.push(json.data[i].brewery.name)		 	}
-		  }
-		})
+nameArr.push(json.data[i].brewery.name)
+		if(json.data[i].brewery.name == "Blue Owl Brewing"){
+			var lng = json.data[i].longitude;
+			var lat = json.data[i].latitude;
+			console.log(json.data[i].brewery.nameShortDisplay);
+			var map;
+      function initMap() {
+        map = new google.maps.Map(document.getElementById('map'), {
+          center: {lat: lat, lng: lng},
+          zoom: 10
+        });
+      }
 		}
-$scope.consoleLog=function(){
+		}
+	}
+})
+		}
+
+$scope.consoleLog = function(){
 	console.log(nameArr)
-}
+}	
 
 
 }])
--- js/main.js
+++ js/main.js
@@ -1,15 +1,5 @@
-var map;
-      function initMap() {
-        map = new google.maps.Map(document.getElementById('map'), {
-          center: {lat: -34.397, lng: 150.644},
-          zoom: 10
-        });
-      }
-      function initMap2(){
-    var map2 = new google.maps.Map(document.getElementById("map2"), {
-    	center: {lat: -29.7604, lng: 95.3698},
-    	zoom: 4
-    })
-    }
 
-//api key AIzaSyBECdr1cotQ82zUZ0BLfU3uZz4hubcnBHE
+
+//api key AIzaSyBECdr1cotQ82zUZ0BLfU3uZz4hubcnBHE -- for google
+
+//api key for brewery 4b50655001c2875f2ef1e4cf9dc31c6c
