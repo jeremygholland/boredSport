d99e23f6962ac28efb9274732788493e2526d857
jeremygholland <jeremygholland@gmail.com>
2018-02-26 18:34:29 -0600

    can now enter a brewery to search

--- js/app.js
+++ js/app.js
@@ -1,39 +1,54 @@
 var app = angular.module('app', [])
 
 app.controller('myCtrl', ['$scope', '$http', function ($scope, $http){
-	$scope.test =  (5*9);
-	console.log($scope.test);
 	var nameArr =[];
+	var lat = [];
+	var lng = [];
+	var map;
+	var userSearch;
+
+      function initMap() {
+      	var brewOne = {lat: lat[0], lng:lng[0]}
+        map = new google.maps.Map(document.getElementById('map'), {
+          center: brewOne,
+          zoom: 17
+        });
+  var marker = new google.maps.Marker({
+    position: brewOne,
+    map: map
+  });
+
+      }
 
 	$scope.btnClick=function(){
-$.ajax
+		userSearch = $('input').val()
+		console.log(userSearch);
+		$.ajax
 			({
 				type: "GET",
 				url: 'https://api.brewerydb.com/v2/search/geo/point?lat=30.2672&lng=-97.7431&radius=30&units=m&key=4b50655001c2875f2ef1e4cf9dc31c6c&format=json',
 				dataType: 'json',
 				success: function(json){
-for (i =0; i<json.data.length; i ++){
-nameArr.push(json.data[i].brewery.name)
-		if(json.data[i].brewery.name == "Blue Owl Brewing"){
-			var lng = json.data[i].longitude;
-			var lat = json.data[i].latitude;
-			console.log(json.data[i].brewery.nameShortDisplay);
-			var map;
-      function initMap() {
-        map = new google.maps.Map(document.getElementById('map'), {
-          center: {lat: lat, lng: lng},
-          zoom: 10
-        });
-      }
-		}
-		}
-	}
-})
+
+					for (i =0; i<json.data.length; i ++){
+						nameArr.push(json.data[i].brewery.name);
+						if(json.data[i].brewery.name == userSearch){
+							lng.push(json.data[i].longitude);
+							lat.push(json.data[i].latitude);
+							console.log(json.data[i].brewery)
+						}
+					}
+				}
+			})
 		}
 
-$scope.consoleLog = function(){
-	console.log(nameArr)
+	$scope.consoleLog = function(){
+		console.log(lng[0]);
+		console.log(lat[0]);
+
+			initMap();
 }	
+
 
 
 }])
--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 
 
+
 //api key AIzaSyBECdr1cotQ82zUZ0BLfU3uZz4hubcnBHE -- for google
 
 //api key for brewery 4b50655001c2875f2ef1e4cf9dc31c6c
